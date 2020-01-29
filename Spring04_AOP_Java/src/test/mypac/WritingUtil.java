4c621e8e73fed209a69924eb37571ab6dc8953a2
Winnie <yeonsu100@gmail.com>
2020-01-29 17:56:39 +0900

    [Spring04_AOP_Java] - add 'WritingUtil'

---
+++ Spring04_AOP_Java/src/test/mypac/WritingUtil.java
@@ -0,0 +1,21 @@
+package test.mypac;
+
+import org.springframework.stereotype.Component;
+
+/*
+ * [bean이 된다]의 의미
+ * - 해당 클래스로 객체가 생성되고 스프링 bean 컨테이너에서 관리가 된다.
+ */
+// @Component 어노테이션을 붙여놓으면 컴포넌트 스캔을 했을 때 bean이 된다.
+@Component
+public class WritingUtil {
+	public void write1() {
+		System.out.println("Write a letter");
+	}
+	public void write2() {
+		System.out.println("Write a diary");
+	}
+	public void write3() {
+		System.out.println("Write a novel");
+	}
+}
