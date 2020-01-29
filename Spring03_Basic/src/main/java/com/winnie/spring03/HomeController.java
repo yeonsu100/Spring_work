a07c5406c51c0d5cebf368b7313a253c46dcc2da
Winnie <yeonsu100@gmail.com>
2020-01-29 11:07:16 +0900

    update Spring03_Basic
    - setting for [*.do]
    - basic setting for [index page]

--- Spring03_Basic/src/main/java/com/winnie/spring03/HomeController.java
+++ Spring03_Basic/src/main/java/com/winnie/spring03/HomeController.java
@@ -1,39 +1,39 @@
 package com.winnie.spring03;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
+/*
+ * @Controller 어노테이션
+ * 
+ * - 해당 클래스가 Spring MVC 프로젝트에서 Controller가 될 수 있도록 한다.
+ * - component scan을 통해서 spring bean container에서 관리가 되는 bean이 되어야 동작을 한다.
  */
 @Controller
-public class HomeController {
+public class HomeController {	
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	// /home.do 요청이 왔을 때 요청을 처리하게하는 @RequestMapping 어노테이션
+	@RequestMapping("/home.do")
+	public String home(HttpServletRequest request) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		List<String> notice=new ArrayList<>();
+		notice.add("Have yourself a well health!");
+		notice.add("Watch out for Wuhan coronavirus");
+		notice.add("Good Luck");
 		
-		String formattedDate = dateFormat.format(date);
+		// 모델을 request에 담는다
+		request.setAttribute("notice", notice);
 		
-		model.addAttribute("serverTime", formattedDate );
-		
+		/* 
+		 * "home"을 리턴해주면
+		 * "/WEB-INF/views/"+"home"+".jsp"와 같은 문자열이 만들어지고
+		 *  /WEB-INF/views/home.jsp 페이지로 forward 이동되어서 응답된다.
+		 */
 		return "home";
 	}
-	
 }
--- Spring03_Basic/src/main/webapp/WEB-INF/views/home.jsp
+++ Spring03_Basic/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<title>/views/home.jsp</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<h1>Index Page</h1>
+
+<h2>Notice</h2>
+<ul>
+	<c:forEach var="tmp" items="${requestScope.notice }">
+		<li>${tmp }</li>
+	</c:forEach>
+</ul>
+
 </body>
 </html>
