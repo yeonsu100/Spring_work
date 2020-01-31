3489aa8a1066f223181014ddadad8be6d7950e1a
Winnie <yeonsu100@gmail.com>
2020-01-31 17:52:53 +0900

    [Spring_Todo] - add a method of 'insertform'

--- Spring_Todo/src/main/java/com/winnie/springtodo/todo/controller/TodoController.java
+++ Spring_Todo/src/main/java/com/winnie/springtodo/todo/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,5 +36,12 @@ public class TodoController {
 		return "todo/insertform";
 	}
 	
+	@RequestMapping("/todo/insert")
+	public ModelAndView insert(@ModelAttribute("dto") TodoDto dto,
+			ModelAndView mView) {
+		dao.insert(dto);
+		mView.setViewName("/todo/insert"); 
+		return mView;
+	}
 	
 }
