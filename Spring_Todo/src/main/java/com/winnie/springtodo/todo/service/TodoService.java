fd37cd063193ab39bb1848dacd81e10644514bb5
Winnie <yeonsu100@gmail.com>
2020-02-05 19:45:12 +0900

    [Spring_Todo] - (un pushed file2)

---
+++ Spring_Todo/src/main/java/com/winnie/springtodo/todo/service/TodoService.java
@@ -0,0 +1,13 @@
+package com.winnie.springtodo.todo.service;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.winnie.springtodo.todo.dto.TodoDto;
+
+public interface TodoService {
+	public void getList(ModelAndView mView);
+	public void addTodo(TodoDto dto);
+	public void getTodo(ModelAndView mView, int num);
+	public void updateTodo(TodoDto dto);
+	public void deleteTodo(int num);
+}
